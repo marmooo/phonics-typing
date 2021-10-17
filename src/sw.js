d2dd7fab631691d5491d12533f6113e9d146d29c
marmooo <marmooo@users.noreply.github.com>
2021-10-17 13:55:11 +0900

    bump bootstrap from 5.1.2 to 5.1.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-10-15 20:33';
+var CACHE_NAME = '2021-10-17 13:42';
 var urlsToCache = [
   "/phonics-typing/",
   "/phonics-typing/index.js",
@@ -10,8 +10,8 @@ var urlsToCache = [
   "/phonics-typing/mp3/keyboard.mp3",
   "/phonics-typing/favicon/original.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/simple-keyboard@latest/build/index.js",
   "https://cdn.jsdelivr.net/npm/simple-keyboard@latest/build/css/index.css",
 ];
